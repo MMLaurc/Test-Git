05751ef89900ac3e0f152a3692eff90f1e1cfd15
Marian Mihaila <151676189+MMLaurc@users.noreply.github.com>
2024-03-31 21:25:57 +0100

    Add files via upload

---
+++ if.py
@@ -0,0 +1,13 @@
+age = int(input("What is your age? : "))
+
+if age == 0:
+    print("You are not borne yet!")
+elif age <= 7:
+    print("Listen your parents!")
+elif age <= 18:
+    print("Still you have to learn go at school!")
+elif age <= 45:
+    print("You are on track! Go on!")
+elif age <= 50:
+    print("You get older!")
+    
